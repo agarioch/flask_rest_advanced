69c45187aa6883c4756603c5b9381c57abf3aa7d
agarioch <alistairgarioch@gmail.com>
2020-06-07 11:14:27 +0100

    Add type hinting including custom types

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
-from typing import Dict
+from typing import Dict, Union
 
 from db import db
+
+UserJSON = Dict[str, Union[int, str]]
 
 
 class UserModel(db.Model):
@@ -13,7 +15,7 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
-    def json(self) -> Dict:
+    def json(self) -> UserJSON:
         return {
             "id": self.id,
             "username": self.username,
@@ -21,11 +23,11 @@ class UserModel(db.Model):
         }
 
     @classmethod
-    def find_by_username(cls, username: str):
+    def find_by_username(cls, username: str) -> "UserModel":
         return cls.query.filter_by(username=username).first()
 
     @classmethod
-    def find_by_id(cls, _id: int):
+    def find_by_id(cls, _id: int) -> "UserModel":
         return cls.query.filter_by(id=_id).first()
 
     def save_to_db(self) -> None:
